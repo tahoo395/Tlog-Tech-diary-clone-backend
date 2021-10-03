b65950bb25691d820dfdaa071549965951a0fcf0
“Tahmid <tahmidahnafpro123@gmail.com>
2021-10-03 15:51:43 +0600

    try to fix cors issue

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ let app = express();
 // middleware setup
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "origin");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
     next();
   });
 
