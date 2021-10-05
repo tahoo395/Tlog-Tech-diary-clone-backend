6045291cb9fbf5b94a9235f5ad5e0aafde1a0de0
“Tahmid <tahmidahnafpro123@gmail.com>
2021-10-05 21:11:03 +0600

    try to fix cors issue

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors({
     credentials: true,
     origin: ["http://localhost:3000" , "https://tlog-techdiary-clone.netlify.app"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
+    preflightContinue: true,
     optionsSuccessStatus: 204
 }))
 app.use(express.json())
